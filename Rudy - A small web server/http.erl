fa860ce6d2cbacf47d44615e3e6cbecce3a34d01
Emil Ståhl <emil.stahl@icloud.com>
2019-09-05 16:41:06 +0200

    localhost shows white

--- Rudy - A small web server/http.erl
+++ Rudy - A small web server/http.erl
@@ -1,48 +1,58 @@
 -module(http).
 
--export([get/1, ok/1, parse_request/1]).
+-export([parse_request/1, ok/1, get/1]).
 
 % Request = Request-Line *(( general-header | request-header
 % | entity-header ) CRLF) CRLF [ message-body ]
 
 parse_request(R0) ->
-    {Request, R1} = request_line(R0),
-    {Headers, R2} = headers(R1),
-    {Body, _} = message_body(R2),
-    {Request, Headers, Body}.
+	{Request, R1} = request_line(R0),
+	{Headers, R2} = headers(R1),
+	{Body, _} = message_body(R2),
+	{Request, Headers, Body}.
 
 % Request-Line = Method SP Request-URI SP HTTP-Version CRLF
 
-request_line([$G, $E, $T, 32 | R0]) ->
-    {URI, R1} = request_uri(R0),
-    {Ver, R2} = http_version(R1),
-    [13, 10 | R3] = R2,
-    {{get, URI, Ver}, R3}.
+request_line([$G, $E, $T, 32 |R0]) ->
+	{URI, R1} = request_uri(R0),
+	{Ver, R2} = http_version(R1),
+	[13,10|R3] = R2,
+	{{get, URI, Ver}, R3}.
 
-request_uri([32 | R0]) -> {[], R0};
-request_uri([C | R0]) ->
-    {Rest, R1} = request_uri(R0), {[C | Rest], R1}.
+request_uri([32|R0])->
+	{[], R0};
+request_uri([C|R0]) ->
+	{Rest, R1} = request_uri(R0),
+	{[C|Rest], R1}.
 
 http_version([$H, $T, $T, $P, $/, $1, $., $1 | R0]) ->
-    {v11, R0};
+	{v11, R0};
 http_version([$H, $T, $T, $P, $/, $1, $., $0 | R0]) ->
-    {v10, R0}.
+	{v10, R0}.
 
 % Parsing of headers
 
-headers([13, 10 | R0]) -> {[], R0};
+headers([13,10|R0]) ->
+	{[],R0};
 headers(R0) ->
-    {Header, R1} = header(R0),
-    {Rest, R2} = headers(R1),
-    {[Header | Rest], R2}.
+	{Header, R1} = header(R0),
+	{Rest, R2} = headers(R1),
+	{[Header|Rest], R2}.
 
-header([13, 10 | R0]) -> {[], R0};
-header([C | R0]) ->
-    {Rest, R1} = header(R0), {[C | Rest], R1}.
+header([13,10|R0]) ->
+	{[], R0};
+header([C|R0]) ->
+	{Rest, R1} = header(R0),
+	{[C|Rest], R1}.
 
 % Body
-message_body(R) -> {R, []}.
+message_body(R) ->
+	{R, []}.
 
-ok(Body) -> "HTTP/1.1 200 OK\r\n" ++ "\r\n" ++ Body.
+% HTTP Reply
+ok(Body) ->
+	"HTTP/1.1 200 OK\r\n" ++ "\r\n" ++ Body.
 
-get(URI) -> "Get " ++ URI ++ " HTTP/1.1\r\n" ++ "\r\n".
+% Generating request
+get(URI) ->
+"GET " ++ URI ++ " HTTP/1.1\r\n" ++ "\r\n".
--- Rudy - A small web server/rudy.erl
+++ Rudy - A small web server/rudy.erl
@@ -3,35 +3,43 @@
 -export([start/1, stop/0]).
 
 start(Port) ->
-    register(rudy, spawn(fun () -> init(Port) end)).
+	register(rudy, spawn(fun() -> init(Port) end)).
 
-stop() -> exit(whereis(rudy), "time to die").
+stop() ->
+	exit(whereis(rudy), "time to die").
 
 init(Port) ->
-    Opt = [list, {active, false}, {reuseadrr, true}],
-    case gen_tcp:listen(Port, Opt) of
-      {ok, Listen} ->
-	  handler(Listen), gen_tcp:close(Listen), ok;
-      {error, _Error} -> error
-    end.
+	Opt = [list, {active, false}, {reuseaddr, true}],
+	case gen_tcp:listen(Port, Opt) of	% Opening the listening socket
+		{ok, Listen} ->
+			handler(Listen),
+			gen_tcp:close(Listen),		% Closing the socket
+			ok;
+		{error, _Error} ->
+			error
+ 	end.
 
 handler(Listen) ->
-    case gen_tcp:accept(Listen) of
-      {ok, Client} -> request(Client), handler(Listen);
-      {error, _Error} -> error
-    end.
+	case gen_tcp:accept(Listen) of		% Accepting incoming request
+		{ok, Client} ->
+			request(Client),
+			handler(Listen);
+		{error, _Error} ->
+			error
+	end.
 
 request(Client) ->
-    Recv = gen_tcp:recv(Client, 0),
-    case Recv of
-      {ok, Str} ->
-	  Request = http:parse_request(Str),
-	  Response = reply(Request),
-	  gen_tcp:send(Client, Response);
-      {error, Error} ->
-	  io:format("Rudy : error ~w~n", [Error])
-    end,
-    gen_tcp:close(Client).
+	Recv = gen_tcp:recv(Client, 0),		% Read the input to string
+	case Recv of
+		{ok, Str} ->
+			Request = http:parse_request(Str),
+			Response = reply(Request),
+			gen_tcp:send(Client, Response);
+		{error, Error} ->
+			io:format("rudy: error: ~w~n", [Error])
+	end,
+	gen_tcp:close(Client).				% Close the connection
 
 reply({{get, _, _}, _, Body}) ->
-    timer:sleep(40), http:ok(Body).
+	timer:sleep(40),
+	http:ok(Body).
