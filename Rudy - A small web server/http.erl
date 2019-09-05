aa9f20446595cc5339d3763b3e2027a71164f684
Emil Ståhl <emil.stahl@icloud.com>
2019-09-05 13:03:31 +0200

    Request works

--- Rudy - A small web server/http.erl
+++ Rudy - A small web server/http.erl
@@ -12,21 +12,25 @@ parse_request(R0) ->
     {Request, Headers, Body}.
 
 % Request-Line = Method SP Request-URI SP HTTP-Version CRLF
-%13,10 is the ASCII code for LF and CR
 
-request_line ( [ $G , $E , $T , 32 | RO ] ) -> { URI , R1 } = request_uri ( R0 ) , { Ver , R2 } = http_version ( R1 ) , { 13 , 10 | R3 } = R2 , { { get , URI , Ver } , R3 } .
-
+request_line([$G, $E, $T, 32 | R0]) ->
+    {URI, R1} = request_uri(R0),
+    {Ver, R2} = http_version(R1),
+    [13, 10 | R3] = R2,
+    {{get, URI, Ver}, R3}.
 
 request_uri([32 | R0]) -> {[], R0};
 request_uri([C | R0]) ->
     {Rest, R1} = request_uri(R0), {[C | Rest], R1}.
 
-http_version([$H, $T, $T, $P, $/, $1, $., $1 | RO]) ->
+http_version([$H, $T, $T, $P, $/, $1, $., $1 | R0]) ->
     {v11, R0};
-http_version([$H, $T, $T, $P, $/, $1, $., $0 | RO]) ->
+http_version([$H, $T, $T, $P, $/, $1, $., $0 | R0]) ->
     {v10, R0}.
 
-headers([13, 10 | RO]) -> {[], R0};
+% Parsing of headers
+
+headers([13, 10 | R0]) -> {[], R0};
 headers(R0) ->
     {Header, R1} = header(R0),
     {Rest, R2} = headers(R1),
@@ -36,4 +40,5 @@ header([13, 10 | R0]) -> {[], R0};
 header([C | R0]) ->
     {Rest, R1} = header(R0), {[C | Rest], R1}.
 
+% Body
 message_body(R) -> {R, []}.
