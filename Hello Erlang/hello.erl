2ae1a2d873779ba0df3d35d9241cb73299b9d9fe
Emil Ståhl <emil.stahl@icloud.com>
2019-09-09 16:04:28 +0200

    hello commits

--- Hello Erlang/hello.erl
+++ Hello Erlang/hello.erl
@@ -1,23 +1,6 @@
 -module(hello).
--export([first/0]).
-
-first() ->
-        receive
-          {tic, X} ->
-              io:format("tic: ~w~n", [X]),
-              second()
-    end.
-    second() ->
-        receive
-          {tac, X} ->
-              io:format("tac: ~w~n", [X]),
-              
-    last();
-          {toe, X} ->
-              io:format("toe: ~w~n", [X]),
-    last() end.
-    last() ->
-        receive
-          X ->
-              io:format("end: ~w~n", [X])
+-export([hello/0]).
+hello() ->
+    receive
+      X -> io:format("aaa! surprise, a message: ~s~n", [X])
     end.
