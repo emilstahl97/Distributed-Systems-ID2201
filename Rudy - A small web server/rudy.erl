ef65540a7246cdd49054cf13510fd9e796df7c6a
Emil Ståhl <emil.stahl@icloud.com>
2019-09-09 17:37:23 +0200

    Finall comments

--- Rudy - A small web server/rudy.erl
+++ Rudy - A small web server/rudy.erl
@@ -1,3 +1,8 @@
+%This is a simple progrm that waits for an incoming request, 
+%delivers a reply and then terminates.
+% @Emil Stahl
+% Date: September 9th 2019
+
 -module(rudy).
 
 -export([start/1, stop/0]).
@@ -5,39 +10,42 @@
 start(Port) ->
     register(rudy, spawn(fun () -> init(Port) end)).
 
-stop() -> exit(whereis(rudy), "time to die").
+stop() -> exit(whereis(rudy), "time to die"). %terminate server
 
 init(Port) ->
     Opt = [list, {active, false}, {reuseaddr, true}],
     case gen_tcp:listen(Port, Opt)       % Opening the listening socket
 	of
       {ok, Listen} ->
-	  handler(Listen),
+	  handler(Listen),                % pass socket to handler/1
 	  gen_tcp:close(Listen),          % Closing the socket
 	  ok;
       {error, _Error} -> error
     end.
 
+% will listen to the socket for an incoming connection
 handler(Listen) ->
     case
       gen_tcp:accept(Listen)          % Accepting incoming request
 	of
-      {ok, Client} -> request(Client), handler(Listen);
+      {ok, Client} -> 
+          request(Client), % pass connection to request/1
+          handler(Listen);
       {error, _Error} -> error
     end.
 
+% will read the request from the client connection and parse it
 request(Client) ->
-    Recv = gen_tcp:recv(Client,
-			0),         % Read the input to string
+    Recv = gen_tcp:recv(Client, 0),         % Read all of the input to string
     case Recv of
       {ok, Str} ->
-	  Request = http:parse_request(Str),
-	  Response = reply(Request),
-	  gen_tcp:send(Client, Response);
+	  Request = http:parse_request(Str), %store the string in Request
+	  Response = reply(Request),        %pass the request to reply
+	  gen_tcp:send(Client, Response);   %send the response to client socket
       {error, Error} ->
 	  io:format("rudy: error: ~w~n", [Error])
     end,
-    gen_tcp:close(Client).                          % Close the connection
+    gen_tcp:close(Client).    % Close the connection
 
 reply({{get, URI, _}, _, _}) ->
     timer:sleep(40), http:ok(URI).
