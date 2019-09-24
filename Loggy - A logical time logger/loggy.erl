5fdd58f9c13f07ceff84df1396bd417ad0e5e431
Emil Ståhl <emil.stahl@icloud.com>
2019-09-24 14:07:46 +0200

    Added sze of HBQ

--- Loggy - A logical time logger/loggy.erl
+++ Loggy - A logical time logger/loggy.erl
@@ -9,30 +9,47 @@ stop(Logger) ->
    	Logger ! stop.
 
 init(Nodes) ->
-    loop(time:clock(Nodes), []).
+	loop(time:clock(Nodes), []).
 
-loop(Clock, MessageQueue) ->
-    receive
-        {log, From, Time, Msg} ->
-			UpdatedClock = time:update(From,Time,Clock),
-			UpdatedQueue = lists:keysort(2, [{From, Time, Msg} | MessageQueue]),
-	    	NewQueue = checkQueue(UpdatedQueue, UpdatedClock, []),
-            loop(UpdatedClock, NewQueue);
-        stop ->
-			SortedQueue = lists:keysort(2, MessageQueue),
-			lists:foreach(fun({From, Time, Msg}) -> log(From, Time, Msg) end, SortedQueue)
+loop(Clock, HoldBackQueue) ->
+	receive
+		{log, From, Time, Msg} ->
+			%% Update the clock when we receive a message 
+			UpdatedClock = time:update(From, Time, Clock),
+			
+			%% Add the new message to the hold-back queue
+			UpdatedHBQ = [{From, Time, Msg}|HoldBackQueue],
+			
+			%% Sort the hold-back queue (needs to be sorted for safe function to work)
+			SortedHBQ = lists:keysort(2, UpdatedHBQ),
+			
+			%% Check if it is safe to log messages and log if it is. Returns the messages that are not safe to log.
+			NewHBQ = checkSafetyAndLog(SortedHBQ, UpdatedClock, []),
+			
+			loop(UpdatedClock, NewHBQ);
+		stop ->
+			io:format("\nSize of Holdback Queue: ~w~n", [length(HoldBackQueue)]),
+			ok
 	end.
 
+%% Log the message, which in this case just means printing it. 
 log(From, Time, Msg) ->
-    io:format("log: ~w ~w ~p~n", [Time, From, Msg]).
+	io:format("log: ~w ~w ~p~n", [Time, From, Msg]).
 
-checkQueue([], _, Queue) ->
-    Queue;
-checkQueue([{From,Time,Msg}|R], Clock, Queue) ->
-    case time:safe(Time, Clock) of
+%% Check if it is safe to log the message, and if it is, do it. Otherwise, keep it in the hold-back queue as it is not yet safe to print. 
+checkSafetyAndLog([], _, Acc)->
+	Acc;
+checkSafetyAndLog([{From, Time, Msg}|T], Clock, Acc)->
+	%% Check safety
+	case time:safe(Time, Clock) of
+		%% If it is safe
 		true ->
-	    	log(From, Time, Msg),
-	  		checkQueue(R, Clock, Queue);
-		false ->
-	    	checkQueue(R, Clock, [{From, Time, Msg} | Queue])
-    end.
+			%% Log message
+			log(From, Time, Msg),
+			%% Check rest of the queue
+			checkSafetyAndLog(T, Clock, Acc);
+		false->
+			%% Check the rest of the list and add the message that is not safe to log to the accumulator
+			checkSafetyAndLog(T, Clock, [{From, Time, Msg}|Acc])
+	end.
+
