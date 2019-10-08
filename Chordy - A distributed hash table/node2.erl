a95036d6a8795750bf082ea51044f074e3bad72b
Emil Ståhl <emil.stahl@icloud.com>
2019-10-08 15:24:08 +0200

    nicer print

--- Chordy - A distributed hash table/node2.erl
+++ Chordy - A distributed hash table/node2.erl
@@ -143,7 +143,7 @@ create_probe(Id, {Skey, Spid}) ->
 % calculate the time it took to pass a probe message around the ring
 remove_probe(Time, Nodes) ->
   T = erlang:system_time(micro_seconds) - Time,
-  io:format("Probe time: ~w Nodes: ~w", [T, Nodes]).
+  io:format("Probe time: ~w micro seconds ~n Nodes: ~w", [T, Nodes]).
 
 % forward the probe message to the successor
 forward_probe(Ref, Time, Nodes, Id, {_, Spid}) ->
